33f985629e57c5248e90b40590eb8fc61f8c4882
Sony Kurian <67534990+sonykurian96@users.noreply.github.com>
2022-09-04 13:03:32 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,51 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:spotifyui/state_management/notify.dart';
 
 import './p/premium.dart';
 import './l/library.dart';
 import './s/search.dart';
 import './h/homepage.dart';
-import 'utils/audio.dart';
-// ignore: import_of_legacy_library_into_null_safe
+
+import 'package:awesome_notifications/awesome_notifications.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
+  AwesomeNotifications().initialize(null, [
+    // notification icon
+    NotificationChannel(
+      channelGroupKey: 'basic_test',
+      channelKey: 'basic',
+      channelName: 'Basic notifications',
+      channelDescription: 'Notification channel for basic tests',
+      channelShowBadge: true,
+      importance: NotificationImportance.High,
+      enableVibration: false,
+    ),
+  ]);
 
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Notify notify = Get.put(Notify());
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
